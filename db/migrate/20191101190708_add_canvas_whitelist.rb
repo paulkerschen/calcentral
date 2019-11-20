293bbba7415fcaf30116da91c7e343673339786d
Paul Kerschen <kerschen@berkeley.edu>
2019-11-20 12:39:49 -0800

    CLC-7135 Make whitelist migration reversible

--- db/migrate/20191101190708_add_canvas_whitelist.rb
+++ db/migrate/20191101190708_add_canvas_whitelist.rb
@@ -1,6 +1,6 @@
 class AddCanvasWhitelist < ActiveRecord::Migration
   def change
-    remove_column :user_auths, :is_author
+    remove_column :user_auths, :is_author, :boolean, null: false, default: false
     add_column :user_auths, :is_canvas_whitelisted, :boolean, null: false, default: false
   end
 end
