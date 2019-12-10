02f766de0050f5e81c549ed234ad46a82a3d70a7
John Crossman <johncrossman@berkeley.edu>
2019-12-10 15:17:05 -0800

    CLC-7167, security vulnerabilities: angular, serialize-javascript; remove React deps

--- src/assets/javascripts/angular/calcentral.js
+++ src/assets/javascripts/angular/calcentral.js
@@ -8,7 +8,6 @@ angular.module('calcentral.controllers', []);
 angular.module('calcentral.directives', []);
 angular.module('calcentral.factories', []);
 angular.module('calcentral.filters', []);
-// angular.module('calcentral.react', []);
 angular.module('calcentral.services', ['ng']);
 angular.module('templates', []);
 
@@ -19,7 +18,6 @@ angular.module('calcentral', [
   'calcentral.directives',
   'calcentral.factories',
   'calcentral.filters',
-  // 'calcentral.react',
   'calcentral.services',
   'ngRoute',
   'ngSanitize',
