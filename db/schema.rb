8966894b901c85066460f630d098953849b29bf7
Paul Kerschen <kerschen@berkeley.edu>
2019-12-19 11:52:24 -0800

    CLC-7187 Add L&S Arts and Humanities HUM courses to OEC

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191101190708) do
+ActiveRecord::Schema.define(version: 20191219114410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
