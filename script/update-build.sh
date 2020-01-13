3a24d67784e8bed401007d9aa3d194c1b5803ecc
John Crossman <johncrossman@berkeley.edu>
2020-01-12 19:07:30 -0800

    CLC-7147, no aws cli dependency; script cleanup

--- script/update-build.sh
+++ script/update-build.sh
@@ -12,15 +12,16 @@ LOGIT="tee -a ${LOG}"
 echo | ${LOGIT}
 
 DEFAULT_DEPLOY_PROPERTIES="${PWD}/config/settings/junction-deploy.properties"
-JUNCTION_DEPLOY_PROPERTIES="${HOME}/.calcentral_config/junction-deploy.properties"
+DEPLOY_PROPERTIES="${HOME}/.calcentral_config/junction-deploy.properties"
+DEPLOYMENT_SUMMARY_FILE="${HOME}/.calcentral_config/.junction-deployment-summary"
 
 if [ ! -f "${DEFAULT_DEPLOY_PROPERTIES}" ]; then
   echo "$(date): [ERROR] Missing properties file: ${DEFAULT_DEPLOY_PROPERTIES}" | ${LOGIT}
   exit 1
 fi
 
-if [ ! -f "${JUNCTION_DEPLOY_PROPERTIES}" ]; then
-  echo "$(date): [ERROR] Missing properties file: ${JUNCTION_DEPLOY_PROPERTIES}" | ${LOGIT}
+if [ ! -f "${DEPLOY_PROPERTIES}" ]; then
+  echo "$(date): [ERROR] Missing properties file: ${DEPLOY_PROPERTIES}" | ${LOGIT}
   exit 1
 fi
 
@@ -28,9 +29,9 @@ function getDeployProperty {
   # Default properties file
   PROPERTIES="${DEFAULT_DEPLOY_PROPERTIES}"
 
-  if [ "$(grep -c "^${1}=" "${JUNCTION_DEPLOY_PROPERTIES}")" -ne '0' ]; then
+  if [ "$(grep -c "^${1}=" "${DEPLOY_PROPERTIES}")" -ne '0' ]; then
     # If value found in custom properties file then use it.
-    PROPERTIES="${JUNCTION_DEPLOY_PROPERTIES}"
+    PROPERTIES="${DEPLOY_PROPERTIES}"
   fi
 
   grep "^${1}=" "${PROPERTIES}" | cut -d'=' -f2
@@ -40,14 +41,24 @@ function getDeployProperty {
 aws_access_key_id=$(getDeployProperty 'aws.access.key')
 aws_secret_access_key=$(getDeployProperty 'aws.secret.access')
 aws_s3_bucket=$(getDeployProperty 'aws.s3.bucket')
-junction_git_branch=$(getDeployProperty 'junction.git.branch')
-junction_git_remote=$(getDeployProperty 'junction.git.remote')
+git_branch=$(getDeployProperty 'junction.git.branch')
+git_remote=$(getDeployProperty 'junction.git.remote')
+
+# Used by curl when downloading files from AWS S3
+date_rfc_2822=$(date -R)
 
 # Get feature flag setting. Is the new S3-based deployment strategy enabled?
 FEATURE_FLAG_S3_DEPLOY=$(getDeployProperty 'feature.flag.s3.deploy' | awk '{print tolower($0)}')
 FEATURE_FLAG_S3_DEPLOY="$(echo -e "${FEATURE_FLAG_S3_DEPLOY}" | tr -d '[:space:]')"
 
+echo "$(date): [INFO] FEATURE_FLAG_S3_DEPLOY='${FEATURE_FLAG_S3_DEPLOY}' " | ${LOGIT}
+
 if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
+
+  if [[ -z "${aws_access_key_id}" || -z "${aws_secret_access_key}" || -z "${aws_s3_bucket}" || -z "${git_branch}" ]]; then
+    echo "$(date): [ERROR] One or more required settings not found in ${DEPLOY_PROPERTIES}."
+    exit 1
+  fi
 
   # We will abort if no update is necessary.
   # I.e., the latest knob file in S3 is identical to what is running in this enviroment.
@@ -59,20 +70,40 @@ if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
   fi
 
   if [ "${junction_knob_id}" == 'latest' ]; then
-    # In non-production environments, pull the latest knob file from AWS S3 bucket.
-    # EXAMPLE of knob file S3 path: s3://rtl-travis-junction/master/20191230_235901Z/calcentral.knob
-    KNOB_FILE_ID=$(aws s3 ls "s3://${aws_s3_bucket}/${junction_git_branch}/" --profile travis | sort --reverse | head -1)
-    # shellcheck disable=SC2001
-    KNOB_FILE_ID=$(echo "${KNOB_FILE_ID}" | sed 's/^.* \(2.*Z\).*/\1/')
+
+    content_type="text/plain"
+    s3_path="${git_branch}/latest.txt"
+    signature=$(echo -en "GET\n\n${content_type}\n${date_rfc_2822}\n/${aws_s3_bucket}/${s3_path}" | openssl sha1 -hmac ${aws_secret_access_key} -binary | base64)
+
+    curl -H "Host: ${aws_s3_bucket}.s3.amazonaws.com" \
+         -H "Date: ${date_rfc_2822}" \
+         -H "Content-Type: ${content_type}" \
+         -H "Authorization: AWS ${aws_access_key_id}:${signature}" \
+         "https://${aws_s3_bucket}.s3.amazonaws.com/${s3_path}" -o latest.txt
+
+    latest_knob_file_id=$(head -1 latest.txt)
+    latest_knob_file_id=${latest_knob_file_id#"/${git_branch}/"}
+    latest_knob_file_id=${latest_knob_file_id%"/calcentral.knob"}
+
+    if [ -z "${latest_knob_file_id}" ]; then
+      echo "$(date): [ERROR] The file s3://${aws_s3_bucket}/${git_branch}/latest.txt has bad data."  | ${LOGIT}
+      exit 1
+    else
+      knob_file_id="${latest_knob_file_id}"
+    fi
+
+    echo "$(date): [INFO] The latest knob file in S3 is ${git_branch}/${knob_file_id}/calcentral.knob." | ${LOGIT}
 
   else
-    # In production, the version of knob file will be specified in JUNCTION_DEPLOY_PROPERTIES (see file info above)
-    KNOB_FILE_ID="${junction_knob_id}"
+    # In production, the version of knob file will be specified in DEPLOY_PROPERTIES (see file info above)
+    knob_file_id="${junction_knob_id}"
   fi
 
-  if [ -f "{JUNCTION_KNOB_FILE_INFO}" ] && [ "$(grep -c "${KNOB_FILE_ID}" "${JUNCTION_KNOB_FILE_INFO}")" -ne '0' ]; then
+  if [ -f "{DEPLOYMENT_SUMMARY_FILE}" ] && [ "$(grep -c "${latest_knob_file_id}" "${DEPLOYMENT_SUMMARY_FILE}")" -ne '0' ]; then
     echo "$(date): [INFO] No update necessary. The latest knob file in S3 is identical to what is currently running in this enviroment." | ${LOGIT}
     exit 0
+  else
+    echo "$(date): [INFO] From AWS S3, we will deploy '${knob_file_id}/calcentral.knob'" | ${LOGIT}
   fi
 fi
 
@@ -84,12 +115,12 @@ source .rvmrc
 
 # Update source tree (from which these scripts run)
 echo "$(date): [INFO] =========================================" | ${LOGIT}
-echo "$(date): [INFO] Updating Junction source code from: ${junction_git_remote}, branch: ${junction_git_branch}" | ${LOGIT}
+echo "$(date): [INFO] Updating Junction source code from: ${git_remote}, branch: ${git_branch}" | ${LOGIT}
 
-git fetch "${junction_git_remote}" 2>&1 | ${LOGIT}
-git fetch -t "${junction_git_remote}" 2>&1 | ${LOGIT}
+git fetch "${git_remote}" 2>&1 | ${LOGIT}
+git fetch -t "${git_remote}" 2>&1 | ${LOGIT}
 git reset --hard HEAD 2>&1 | ${LOGIT}
-git checkout -qf "${junction_git_branch}" 2>&1 | ${LOGIT}
+git checkout -qf "${git_branch}" 2>&1 | ${LOGIT}
 
 echo "$(date): [INFO] Last commit in source tree:" | ${LOGIT}
 git log -1 | ${LOGIT}
@@ -110,17 +141,18 @@ echo "------------------------------------------" | ${LOGIT}
 
 if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
 
-  if [[ -z "${aws_access_key_id}" || -z "${aws_secret_access_key}" || -z "${aws_s3_bucket}" || -z "${junction_git_branch}" ]]; then
-    echo "$(date): [ERROR] One or more required settings not found in ${JUNCTION_DEPLOY_PROPERTIES}."
-    exit 1
-  fi
-  # Download from S3
-  export AWS_ACCESS_KEY_ID="${aws_access_key_id}"
-  export AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}"
-  s3_path="s3://${aws_s3_bucket}/${junction_git_branch}/${KNOB_FILE_ID}/calcentral.knob"
+  content_type="application/x-compressed-tar"
+  s3_path="${git_branch}/${knob_file_id}/calcentral.knob"
+  signature=$(echo -en "GET\n\n${content_type}\n${date_rfc_2822}\n/${aws_s3_bucket}/${s3_path}" | openssl sha1 -hmac ${aws_secret_access_key} -binary | base64)
 
+  # Download from S3
   echo "$(date): [INFO] Fetching new calcentral.knob from ${s3_path}" | ${LOGIT}
-  aws s3 cp "${s3_path}" .
+
+  curl -H "Host: ${aws_s3_bucket}.s3.amazonaws.com" \
+       -H "Date: ${date_rfc_2822}" \
+       -H "Content-Type: ${content_type}" \
+       -H "Authorization: AWS ${aws_access_key_id}:${signature}" \
+       "https://${aws_s3_bucket}.s3.amazonaws.com/${s3_path}" -o calcentral.knob
 
 else
   echo "$(date): [INFO] Fetching new calcentral.knob from ${WAR_URL}" | ${LOGIT}
@@ -167,8 +199,10 @@ cp -Rvf public/canvas ${DOC_ROOT} | ${LOGIT}
 echo "$(date): [INFO] Copying OAuth static files into ${DOC_ROOT}" | ${LOGIT}
 cp -Rvf public/oauth ${DOC_ROOT} | ${LOGIT}
 
-# Keep a record of what was deployed
-echo "${KNOB_FILE_ID}" > "${JUNCTION_KNOB_FILE_INFO}"
+if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
+  # Keep a record of what was deployed
+  echo "${knob_file_id}" > "${DEPLOYMENT_SUMMARY_FILE}"
+fi
 
 echo "$(date): [INFO] Congratulations, deployment complete." | ${LOGIT}
 
