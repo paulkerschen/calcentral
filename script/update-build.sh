c75384217612f720ff27c735966b61bb6719240e
John Crossman <johncrossman@berkeley.edu>
2020-01-21 14:07:43 -0800

    CLC-7192, get-info-on-latest-knob broken off into separate script

--- script/update-build.sh
+++ script/update-build.sh
@@ -9,32 +9,28 @@
 LOG=$(date +"${PWD}/log/update-build_%Y-%m-%d.log")
 LOGIT="tee -a ${LOG}"
 
+function log_error {
+  echo | ${LOGIT}
+  echo "$(date): [ERROR] ${1}" | ${LOGIT}
+  echo | ${LOGIT}
+}
+
+function log_info {
+  echo "$(date): [INFO] ${1}" | ${LOGIT}
+}
+
 echo | ${LOGIT}
 
-DEFAULT_DEPLOY_PROPERTIES="${PWD}/config/settings/junction-deploy.properties"
-DEPLOY_PROPERTIES="${HOME}/.calcentral_config/junction-deploy.properties"
-DEPLOYMENT_SUMMARY_FILE="${HOME}/.calcentral_config/.junction-deployment-summary"
+deploy_properties="${HOME}/.calcentral_config/junction-deploy.properties"
+deployment_summary_file="${HOME}/.calcentral_config/.junction-deployment-summary"
 
-if [ ! -f "${DEFAULT_DEPLOY_PROPERTIES}" ]; then
-  echo "$(date): [ERROR] Missing properties file: ${DEFAULT_DEPLOY_PROPERTIES}" | ${LOGIT}
-  exit 1
-fi
-
-if [ ! -f "${DEPLOY_PROPERTIES}" ]; then
-  echo "$(date): [ERROR] Missing properties file: ${DEPLOY_PROPERTIES}" | ${LOGIT}
+if [ ! -f "${deploy_properties}" ]; then
+  log_error "Missing properties file: ${deploy_properties}"
   exit 1
 fi
 
 function getDeployProperty {
-  # Default properties file
-  PROPERTIES="${DEFAULT_DEPLOY_PROPERTIES}"
-
-  if [ "$(grep -c "^${1}=" "${DEPLOY_PROPERTIES}")" -ne '0' ]; then
-    # If value found in custom properties file then use it.
-    PROPERTIES="${DEPLOY_PROPERTIES}"
-  fi
-
-  grep "^${1}=" "${PROPERTIES}" | cut -d'=' -f2
+  grep "^${1}=" "${deploy_properties}" | cut -d'=' -f2
 }
 
 # The calcentral.knob file will be pulled from S3 bucket in AWS
@@ -48,15 +44,15 @@ git_remote=$(getDeployProperty 'junction.git.remote')
 date_rfc_2822=$(date -R)
 
 # Get feature flag setting. Is the new S3-based deployment strategy enabled?
-FEATURE_FLAG_S3_DEPLOY=$(getDeployProperty 'feature.flag.s3.deploy' | awk '{print tolower($0)}')
-FEATURE_FLAG_S3_DEPLOY="$(echo -e "${FEATURE_FLAG_S3_DEPLOY}" | tr -d '[:space:]')"
+feature_flag_s3_deploy=$(getDeployProperty 'feature.flag.s3.deploy' | awk '{print tolower($0)}')
+feature_flag_s3_deploy="$(echo -e "${feature_flag_s3_deploy}" | tr -d '[:space:]')"
 
-echo "$(date): [INFO] FEATURE_FLAG_S3_DEPLOY='${FEATURE_FLAG_S3_DEPLOY}' " | ${LOGIT}
+log_info "feature_flag_s3_deploy='${feature_flag_s3_deploy}'"
 
-if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
+if [ "${feature_flag_s3_deploy}" == 'true' ]; then
 
   if [[ -z "${aws_access_key_id}" || -z "${aws_secret_access_key}" || -z "${aws_s3_bucket}" || -z "${git_branch}" ]]; then
-    echo "$(date): [ERROR] One or more required settings not found in ${DEPLOY_PROPERTIES}."
+    log_error "One or more required settings not found in ${deploy_properties}."
     exit 1
   fi
 
@@ -65,89 +61,76 @@ if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
   junction_knob_id=$(getDeployProperty 'junction.knob.id')
 
   if [ -z "${junction_knob_id}" ]; then
-    echo "$(date): [ERROR] The 'junction.knob.id' config is blank or missing." | ${LOGIT}
+    log_error "The 'junction.knob.id' config is blank or missing."
     exit 1
   fi
 
   if [ "${junction_knob_id}" == 'latest' ]; then
 
-    content_type="text/plain"
-    s3_path="${git_branch}/latest.txt"
-    signature=$(echo -en "GET\n\n${content_type}\n${date_rfc_2822}\n/${aws_s3_bucket}/${s3_path}" | openssl sha1 -hmac ${aws_secret_access_key} -binary | base64)
-
-    curl -sS \
-         -H "Host: ${aws_s3_bucket}.s3.amazonaws.com" \
-         -H "Date: ${date_rfc_2822}" \
-         -H "Content-Type: ${content_type}" \
-         -H "Authorization: AWS ${aws_access_key_id}:${signature}" \
-         "https://${aws_s3_bucket}.s3.amazonaws.com/${s3_path}" -o latest.txt > /dev/null
-
-    latest_knob_file_id=$(head -1 latest.txt)
-    latest_knob_file_id=${latest_knob_file_id#"/${git_branch}/"}
-    latest_knob_file_id=${latest_knob_file_id%"/calcentral.knob"}
+    latest_knob_file_id=$(./script/deploy/get_latest_knob_file_id.sh)
 
     if [ -z "${latest_knob_file_id}" ]; then
-      echo "$(date): [ERROR] The file s3://${aws_s3_bucket}/${git_branch}/latest.txt has bad data."  | ${LOGIT}
+      log_error "The file s3://${aws_s3_bucket}/${git_branch}/latest.txt has bad data."
       exit 1
     else
       knob_file_id="${latest_knob_file_id}"
     fi
 
-    echo "$(date): [INFO] The latest knob file in S3 is ${git_branch}/${knob_file_id}/calcentral.knob." | ${LOGIT}
+    log_info "The latest knob file in S3 is ${git_branch}/${knob_file_id}/calcentral.knob."
 
   else
-    # In production, the version of knob file will be specified in DEPLOY_PROPERTIES (see file info above)
+    # In production, the version of knob file will be specified in deploy_properties (see file info above)
     knob_file_id="${junction_knob_id}"
   fi
 
-  if [ -f "{DEPLOYMENT_SUMMARY_FILE}" ] && [ "$(grep -c "${latest_knob_file_id}" "${DEPLOYMENT_SUMMARY_FILE}")" -ne '0' ]; then
-    echo "$(date): [INFO] No update necessary. The latest knob file in S3 is identical to what is currently running in this enviroment." | ${LOGIT}
+  if [ -f "{deployment_summary_file}" ] && [ "$(grep -c "${latest_knob_file_id}" "${deployment_summary_file}")" -ne '0' ]; then
+    log_info "No update necessary. The latest knob file in S3 is identical to what is currently running in this enviroment."
     exit 0
   else
-    echo "$(date): [INFO] From AWS S3, we will deploy '${knob_file_id}/calcentral.knob'" | ${LOGIT}
+    log_info "From AWS S3, we will deploy '${knob_file_id}/calcentral.knob'"
   fi
 fi
 
-cd $( dirname "${BASH_SOURCE[0]}" )/..
+cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
 # Enable rvm and use the correct Ruby version and gem set.
 [[ -s "${HOME}/.rvm/scripts/rvm" ]] && . "${HOME}/.rvm/scripts/rvm"
 source .rvmrc
 
 # Update source tree (from which these scripts run)
-echo "$(date): [INFO] =========================================" | ${LOGIT}
-echo "$(date): [INFO] Updating Junction source code from: ${git_remote}, branch: ${git_branch}" | ${LOGIT}
+log_info "========================================="
+log_info "Updating Junction source code from: ${git_remote}, branch: ${git_branch}"
 
 git fetch "${git_remote}" 2>&1 | ${LOGIT}
 git fetch -t "${git_remote}" 2>&1 | ${LOGIT}
 git reset --hard HEAD 2>&1 | ${LOGIT}
 git checkout -qf "${git_branch}" 2>&1 | ${LOGIT}
 
-echo "$(date): [INFO] Last commit in source tree:" | ${LOGIT}
+log_info "Last commit in source tree:"
 git log -1 | ${LOGIT}
 
 
 echo | ${LOGIT}
 echo "------------------------------------------" | ${LOGIT}
-echo "$(date): [INFO] Stopping Junction..." | ${LOGIT}
+log_info "Stopping Junction..."
 
 ./script/stop-torquebox.sh
 
 rm -rf deploy
 mkdir deploy
-cd deploy
+cd deploy || exit 1
 
 echo | ${LOGIT}
 echo "------------------------------------------" | ${LOGIT}
 
-if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
+if [ "${feature_flag_s3_deploy}" == 'true' ]; then
 
   content_type="application/x-compressed-tar"
   s3_path="${git_branch}/${knob_file_id}/calcentral.knob"
   signature=$(echo -en "GET\n\n${content_type}\n${date_rfc_2822}\n/${aws_s3_bucket}/${s3_path}" | openssl sha1 -hmac ${aws_secret_access_key} -binary | base64)
 
   # Download from S3
-  echo "$(date): [INFO] Fetching new calcentral.knob from ${s3_path}" | ${LOGIT}
+  log_info "Fetching new calcentral.knob from ${s3_path}"
 
   curl -sS \
        -H "Host: ${aws_s3_bucket}.s3.amazonaws.com" \
@@ -157,22 +140,22 @@ if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
        "https://${aws_s3_bucket}.s3.amazonaws.com/${s3_path}" -o calcentral.knob > /dev/null
 
 else
-  echo "$(date): [INFO] Fetching new calcentral.knob from ${WAR_URL}" | ${LOGIT}
+  log_info "Fetching new calcentral.knob from ${WAR_URL}"
 
   # Get calcentral.knob file from Bamboo (deprecated deployment strategy)
   WAR_URL=${WAR_URL:="https://bamboo.media.berkeley.edu/bamboo/browse/MYB-MVPWAR/latest/artifact/JOB1/warfile/calcentral.knob"}
   curl -k -s ${WAR_URL} > calcentral.knob
 fi
 
-echo "$(date): [INFO] Unzipping knob..." | ${LOGIT}
+log_info "Unzipping knob..."
 
 jar xf calcentral.knob
 
 if [ ! -d "versions" ]; then
-  echo "$(date): [ERROR] Missing or malformed calcentral.knob file!" | ${LOGIT}
+  log_error "Missing or malformed calcentral.knob file!"
   exit 1
 fi
-echo "$(date): [INFO] Last commit in calcentral.knob:" | ${LOGIT}
+log_info "Last commit in calcentral.knob:"
 cat versions/git.txt | ${LOGIT}
 
 # Fix permissions on files that need to be executable
@@ -182,31 +165,31 @@ find ./vendor/bundle -name standalone.sh | xargs chmod u+x
 
 echo | ${LOGIT}
 echo "------------------------------------------" | ${LOGIT}
-echo "$(date): [INFO] Deploying new Junction knob..." | ${LOGIT}
+log_info "Deploying new Junction knob..."
 
 bundle exec torquebox deploy calcentral.knob --env=production | ${LOGIT}
 
 MAX_ASSET_AGE_IN_DAYS=${MAX_ASSET_AGE_IN_DAYS:="45"}
 DOC_ROOT="/var/www/html/junction"
 
-echo "$(date): [INFO] Copying assets into ${DOC_ROOT}" | ${LOGIT}
+log_info "Copying assets into ${DOC_ROOT}"
 cp -Rvf public/assets ${DOC_ROOT} | ${LOGIT}
 
-echo "$(date): [INFO] Deleting old assets from ${DOC_ROOT}/assets" | ${LOGIT}
+log_info "Deleting old assets from ${DOC_ROOT}/assets"
 find ${DOC_ROOT}/assets -type f -mtime +${MAX_ASSET_AGE_IN_DAYS} -delete | ${LOGIT}
 
-echo "$(date): [INFO] Copying bCourses static files into ${DOC_ROOT}" | ${LOGIT}
+log_info "Copying bCourses static files into ${DOC_ROOT}"
 cp -Rvf public/canvas ${DOC_ROOT} | ${LOGIT}
 
-echo "$(date): [INFO] Copying OAuth static files into ${DOC_ROOT}" | ${LOGIT}
+log_info "Copying OAuth static files into ${DOC_ROOT}"
 cp -Rvf public/oauth ${DOC_ROOT} | ${LOGIT}
 
-if [ "${FEATURE_FLAG_S3_DEPLOY}" == 'true' ]; then
+if [ "${feature_flag_s3_deploy}" == 'true' ]; then
   # Keep a record of what was deployed
-  echo "${knob_file_id}" > "${DEPLOYMENT_SUMMARY_FILE}"
+  echo "${knob_file_id}" > "${deployment_summary_file}"
 fi
 
-echo "$(date): [INFO] Congratulations, deployment complete." | ${LOGIT}
+log_info "Congratulations, deployment complete."
 
 echo | ${LOGIT}
 
