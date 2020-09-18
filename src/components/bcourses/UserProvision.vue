2454e370f137f8c067d736420d27f4fafaa46607
Paul Kerschen <kerschen@berkeley.edu>
2020-09-17 18:05:04 -0700

    CLC-7541 API wiring for user provision LTI tool

--- src/components/bcourses/UserProvision.vue
+++ src/components/bcourses/UserProvision.vue
@@ -4,7 +4,7 @@
       <b-row no-gutters>
         <h1 class="cc-page-user-provision-heading">Add Users to bCourses</h1>
       </b-row>
-      <form name="userImportForm" :submit="importUsers(rawUids)">
+      <form name="userImportForm" @submit="onSubmit">
         <b-row no-gutters>
           <b-col cols="10">
             <b-container fluid>
@@ -26,16 +26,16 @@
                     placeholder="Paste your list of UIDs here organized one UID per a line, or separated by spaces or commas."
                   >
                   </textarea>
-                  <small v-if="checkPerformed && validationErrors.required" class="error">
+                  <small v-if="validationErrors.required" role="alert" aria-live="polite">
                     You must provide at least one
                     <span aria-hidden="true">UID</span>
                     <span class="sr-only">U I D</span>
                     .
                   </small>
-                  <small v-if="validationErrors.ccNumericList" class="error">
+                  <small v-if="validationErrors.ccNumericList" role="alert" aria-live="polite">
                     The following items in your list are not numeric: {{ invalidValues.join(', ') }}
                   </small>
-                  <small v-if="validationErrors.ccListLimit" class="error">
+                  <small v-if="validationErrors.ccListLimit" role="alert" aria-live="polite">
                     Maximum IDs: 200. {{ listLength }} IDs found in list.
                   </small>
                 </b-col>
@@ -51,21 +51,18 @@
                     Import Users
                   </button>
                 </b-col>
-                <b-col v-if="displayImportResult" cols="8">
-                  <div data-cc-spinner-directive>
-                    <div class="row">
-                      <div class="small-11 small-offset-1 columns">
-                        <div class="cc-page-user-provision-feedback" role="alert">
-                          <div v-if="status === 'error'">
-                            <i class="cc-left fa fa-exclamation-circle cc-icon-red"></i>
-                            <strong>Error : {{ error }}</strong>
-                          </div>
-                          <div v-if="status === 'success'">
-                            <i class="cc-left fa fa-check-circle cc-icon-green"></i>
-                            Success : The users specified were imported into bCourses.
-                          </div>
-                        </div>
-                      </div>
+                <b-col cols="8">
+                  <div role="alert" aria-live="polite">
+                    <div v-if="importProcessing" class="cc-spinner">
+                      <span class="sr-only">Processing import</span>
+                    </div>
+                    <div v-if="status === 'error'" class="cc-page-user-provision-feedback">
+                      <fa icon="exclamation-circle" class="cc-icon-red mr-2"></fa>
+                      <strong>Error : {{ error }}</strong>
+                    </div>
+                    <div v-if="status === 'success'" class="cc-page-user-provision-feedback">
+                      <fa icon="check-circle" class="cc-icon-green mr-2"></fa>
+                      Success : The users specified were imported into bCourses.
                     </div>
                   </div>
                 </b-col>
@@ -79,20 +76,63 @@
 </template>
 
 <script>
+import {importUsers} from '@/api/canvas'
+
 export default {
   name: 'UserProvision',
   data: () => ({
-    checkPerformed: false,
     error: null,
+    importProcessing: false,
     importUsers: () => {},
     invalidValues: [],
+    listLength: null,
     rawUids: '',
     status: null,
     validationErrors: {}
   }),
   computed: {
     importButtonDisabled() {
-      return !this.rawUids.length
+      return this.importProcessing || !this.rawUids.length
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      this.error = null
+      const validatedUids = this.validateUids()
+      if (validatedUids) {
+        this.importProcessing = true
+        this.status = null
+        importUsers(validatedUids).then(response => {
+          this.importProcessing = false
+          this.status = response.status
+        }).catch(response => {
+          this.importProcessing = false
+          this.status = 'error'
+          this.error = response.error
+        })
+      }
+    },
+    validateUids() {
+      this.validationErrors = {}
+      this.invalidValues = []
+      const uids = this.rawUids.match(/\w+/g)
+      if (!uids) {
+        this.validationErrors.required = true
+      }
+      this.listLength = uids.length
+      if (this.listLength > 200) {
+        this.validationErrors.ccListLimit = true
+      }
+      this.$_.each(uids, uid => {
+        if (isNaN(Number(uid))) {
+          this.invalidValues.push(uid)
+          this.validationErrors.ccNumericList = true
+        }
+      })
+      if (this.$_.isEmpty(this.validationErrors)) {
+        return uids.join()
+      }
     }
   }
 }
@@ -119,12 +159,13 @@ export default {
 
   .cc-page-user-provision-feedback {
     padding: 5px 0 15px;
-    i {
-      margin-right: 5px;
-    }
-    > div {
+    div {
       margin-top: 5px;
     }
+  }
+
+  small {
+    font-weight: 300;
   }
 }
 </style>
