cecaaf388b4f5211171edb8ec904c85bccb652d9
John Crossman <johncrossman@berkeley.edu>
2020-09-22 11:00:13 -0700

    CLC-7518, download roster CSV

--- src/api/api-utils.ts
+++ src/api/api-utils.ts
@@ -4,5 +4,14 @@ import axios from 'axios'
 export default {
   apiBaseUrl: () => Vue.prototype.$config.apiBaseUrl,
   get: path => axios.get(`${Vue.prototype.$config.apiBaseUrl}${path}`).then(response => response.data),
-  post: (path, data={}) => axios.post(`${Vue.prototype.$config.apiBaseUrl}${path}`, data).then(response => response.data)
+  post: (path, data={}) => axios.post(`${Vue.prototype.$config.apiBaseUrl}${path}`, data).then(response => response.data),
+  downloadViaGet(path: string, filename: string) {
+    const fileDownload = require('js-file-download')
+    return axios.get(`${Vue.prototype.$config.apiBaseUrl}${path}`).then(
+      response => {
+        fileDownload(response.data, filename)
+      },
+      () => null
+    )
+  }
 }
