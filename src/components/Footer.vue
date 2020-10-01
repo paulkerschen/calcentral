929c1cffda9bc44018de8b6db3de72d3bee7d155
John Crossman <johncrossman@berkeley.edu>
2020-10-01 10:36:26 -0700

    CLC-7523, fix footer alignment in Toolbox template

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,96 +1,104 @@
 <template>
-  <b-row class="bg-white cc-print-hide fixed pb-3 pl-3 pr-4 w-100 z-max" no-gutters>
-    <b-row v-if="$currentUser.isDirectlyAuthenticated || !$currentUser.isLoggedIn" class="border-top pl-2 pt-3 text-secondary w-100" no-gutters>
-      <b-col sm="6">
-        <div>
-          Berkeley &copy; {{ new Date().getFullYear() }} UC Regents
-        </div>
-        <div v-if="includeBuildSummary" class="pt-3">
-          <div class="d-flex">
+  <b-row class="cc-print-hide pb-5 w-100" no-gutters>
+    <b-col>
+      <b-container class="fixed pb-3 w-100 z-max">
+        <b-row
+          v-if="$currentUser.isDirectlyAuthenticated || !$currentUser.isLoggedIn"
+          class="mt-3 text-secondary"
+          no-gutters
+        >
+          <b-col sm="6">
             <div>
-              <h4>Build Summary</h4>
+              Berkeley &copy; {{ new Date().getFullYear() }} UC Regents
             </div>
-            <div>
+            <div v-if="includeBuildSummary" class="pt-3">
+              <div class="d-flex">
+                <div>
+                  <h4>Build Summary</h4>
+                </div>
+                <div>
+                  <b-button
+                    id="toggle-show-build-summary"
+                    aria-controls="build-summary-collapse"
+                    variant="link"
+                    @click="showBuildSummary = !showBuildSummary"
+                  >
+                    <fa :icon="showBuildSummary ? 'caret-up' : 'caret-down'" />
+                  </b-button>
+                </div>
+              </div>
+              <b-collapse id="build-summary-collapse" v-model="showBuildSummary" class="ml-3">
+                <BuildSummary />
+              </b-collapse>
+            </div>
+            <div v-if="$currentUser.isBasicAuthEnabled && !$currentUser.isLoggedIn" class="pt-2">
+              <div class="d-flex pb-2">
+                <div>
+                  <h4>DevAuth</h4>
+                </div>
+                <div>
+                  <b-button
+                    id="toggle-show-dev-auth"
+                    aria-controls="dev-auth-collapse"
+                    variant="link"
+                    @click="showDevAuth = !showDevAuth"
+                  >
+                    <fa :icon="showDevAuth ? 'caret-up' : 'caret-down'" />
+                  </b-button>
+                </div>
+              </div>
+              <b-collapse id="dev-auth-collapse" v-model="showDevAuth">
+                <DevAuth />
+              </b-collapse>
+            </div>
+          </b-col>
+          <b-col sm="6">
+            <div class="d-flex flex-wrap float-right">
+              <div>
+                <a href="https://bcourses.berkeley.edu" target="_blank">Return to bCourses<span class="sr-only"> (link opens new browser tab)</span></a>
+              </div>
+              <div class="pl-1 pr-1">|</div>
+              <div>
+                <a href="https://security.berkeley.edu/policy" target="_blank">Usage Policy<span class="sr-only"> (link opens new browser tab)</span></a>
+              </div>
+              <div class="pl-1 pr-1">|</div>
+              <div>
+                <a href="https://www.ets.berkeley.edu/services-facilities/bcourses" target="_blank">About<span class="sr-only"> bCourses (link opens new browser tab)</span></a>
+              </div>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row v-if="$currentUser.isLoggedIn && !$currentUser.isDirectlyAuthenticated" class="border-top pl-3 pt-3 text-secondary w-100" no-gutters>
+          <b-col class="pt-1" sm="8">
+            <div aria-live="polite" role="alert">
+              You are viewing as {{ $currentUser.fullName }} ({{ $currentUser.uid }}),
+              <span v-if="$currentUser.firstLoginAt">first logged in on {{ $currentUser.firstLoginAt | moment('M/D/YY') }}</span>
+              <span v-if="!$currentUser.firstLoginAt">who has never logged in to CalCentral</span>
+            </div>
+          </b-col>
+          <b-col sm="4">
+            <div class="float-right">
               <b-button
-                id="toggle-show-build-summary"
-                aria-controls="build-summary-collapse"
-                variant="link"
-                @click="showBuildSummary = !showBuildSummary"
+                id="stop-viewing-as"
+                class="btn-stop-viewing-as cc-button-blue text-nowrap"
+                size="sm"
+                variant="outline-secondary"
+                @click="stopActAs"
               >
-                <fa :icon="showBuildSummary ? 'caret-up' : 'caret-down'" />
+                Stop viewing as
               </b-button>
             </div>
-          </div>
-          <b-collapse id="build-summary-collapse" v-model="showBuildSummary" class="ml-3">
-            <BuildSummary />
-          </b-collapse>
-        </div>
-        <div v-if="$currentUser.isBasicAuthEnabled && !$currentUser.isLoggedIn" class="pt-2">
-          <div class="d-flex pb-2">
-            <div>
-              <h4>DevAuth</h4>
+          </b-col>
+        </b-row>
+        <b-row v-if="$config.isVueAppDebugMode" class="pl-3 w-100">
+          <b-col sm="12">
+            <div class="text-secondary">
+              <span class="font-weight-bolder">Screen-reader alert:</span> {{ screenReaderAlert || '&mdash;' }}
             </div>
-            <div>
-              <b-button
-                id="toggle-show-dev-auth"
-                aria-controls="dev-auth-collapse"
-                variant="link"
-                @click="showDevAuth = !showDevAuth"
-              >
-                <fa :icon="showDevAuth ? 'caret-up' : 'caret-down'" />
-              </b-button>
-            </div>
-          </div>
-          <b-collapse id="dev-auth-collapse" v-model="showDevAuth">
-            <DevAuth />
-          </b-collapse>
-        </div>
-      </b-col>
-      <b-col sm="6">
-        <div class="d-flex flex-wrap float-right">
-          <div>
-            <a href="https://bcourses.berkeley.edu" target="_blank">Return to bCourses<span class="sr-only"> (link opens new browser tab)</span></a>
-          </div>
-          <div class="pl-1 pr-1">|</div>
-          <div>
-            <a href="https://security.berkeley.edu/policy" target="_blank">Usage Policy<span class="sr-only"> (link opens new browser tab)</span></a>
-          </div>
-          <div class="pl-1 pr-1">|</div>
-          <div>
-            <a href="https://www.ets.berkeley.edu/services-facilities/bcourses" target="_blank">About<span class="sr-only"> bCourses (link opens new browser tab)</span></a>
-          </div>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row v-if="$currentUser.isLoggedIn && !$currentUser.isDirectlyAuthenticated" class="border-top pl-3 pt-3 text-secondary w-100" no-gutters>
-      <b-col class="pt-1" sm="8">
-        <div aria-live="polite" role="alert">
-          You are viewing as {{ $currentUser.fullName }} ({{ $currentUser.uid }}),
-          <span v-if="$currentUser.firstLoginAt">first logged in on {{ $currentUser.firstLoginAt | moment('M/D/YY') }}</span>
-          <span v-if="!$currentUser.firstLoginAt">who has never logged in to CalCentral</span>
-        </div>
-      </b-col>
-      <b-col sm="4">
-        <div class="float-right">
-          <b-button
-            id="stop-viewing-as"
-            class="btn-stop-viewing-as cc-button-blue text-nowrap"
-            size="sm"
-            variant="outline-secondary"
-            @click="stopActAs"
-          >
-            Stop viewing as
-          </b-button>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row v-if="$config.isVueAppDebugMode" class="pl-3 w-100">
-      <b-col sm="12">
-        <div class="text-secondary">
-          <span class="font-weight-bolder">Screen-reader alert:</span> {{ screenReaderAlert || '&mdash;' }}
-        </div>
-      </b-col>
-    </b-row>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-col>
   </b-row>
 </template>
 
@@ -133,7 +141,6 @@ h4 {
 .fixed {
   border-top: 2px solid $cc-color-dark-tangerine;
   bottom: 0;
-  left: 0;
   position: fixed;
   @media print {
     position: relative;
